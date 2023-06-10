b6229619666b14d95b00775cdd1746e059adf2a7
dk-davidekim <dk.davidekim@gmail.com>
2023-06-10 14:19:48 -0400

    minor change - added edit and delete button

--- Application/src/app/arts/art-list/art-list.component.ts
+++ Application/src/app/arts/art-list/art-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from 'rxjs'
 import { Art } from '../art.model';
 import { ArtsService } from "../arts.service";
