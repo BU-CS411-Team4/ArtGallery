b52d9181bb2957425c09259c16c847f1e4b51655
dk-davidekim <dk.davidekim@gmail.com>
2023-06-12 02:08:24 -0400

    backend, router on front & back, database, add and delete capability

--- Application/src/app/app-routing.module.ts
+++ Application/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ArtListComponent} from "./arts/art-list/art-list.component";
+import {ArtCreateComponent} from "./arts/art-create/art-create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ArtListComponent },
+  { path: 'create', component: ArtCreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Application/src/app/arts/arts.service.ts
+++ Application/src/app/arts/arts.service.ts
@@ -1,22 +1,57 @@
-import { Art } from './art.model'
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import {Art} from './art.model'
+import {Injectable} from "@angular/core";
+import {HttpClient} from '@angular/common/http'
+import {Subject} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class ArtsService {
   private arts: Art[] = [];
   private artsUpdated = new Subject<Art[]>();
 
+  constructor(private http: HttpClient) {}
+
   getArts(){
-    return [...this.arts];
+    this.http
+      .get<{ message: string; arts: any }>(
+        'http://localhost:3000/api/arts'
+      )
+      .pipe(map((artData) => {
+        // @ts-ignore
+        return artData.arts.map(art => {
+          return {
+            keyword: art.keyword,
+            artFile: art.artFile,
+            id: art._id
+          }
+        });
+      }))
+      .subscribe((transformedArts) => {
+        this.arts = transformedArts;
+        this.artsUpdated.next([...this.arts]);
+      });
   }
 
   getArtUpdateListener() {
     return this.artsUpdated.asObservable();
   }
 
-  addArt(art: Art) {
-    this.arts.push(art)
-    this.artsUpdated.next([...this.arts])
+  addArt(keyword:string, artFile:string) {
+    const art: Art = {id: '', keyword:keyword, artFile:artFile};
+    this.http
+      .post<{message:string, artId: string}>('http://localhost:3000/api/arts', art)
+      .subscribe((responseData) => {
+        art.id = responseData.artId;
+        this.arts.push(art)
+        this.artsUpdated.next([...this.arts])
+      });
+  }
+
+  deleteArt(artId: string){
+    this.http.delete("http://localhost:3000/api/arts/" + artId)
+      .subscribe(() => {
+        this.arts = this.arts.filter(art => art.id !== artId);
+        this.artsUpdated.next([...this.arts])
+      })
   }
 }
