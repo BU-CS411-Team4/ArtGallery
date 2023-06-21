06fc69f538ff8ce80057ec74a6ba2e42c973a6c5
Victoria B <victoriaboulin@crc-dot1x-nat-10-239-87-185.bu.edu>
2023-06-21 10:13:18 -0400

    google oauth

--- Application/src/app/app-routing.module.ts
+++ Application/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ArtListComponent} from "./arts/art-list/art-list.component";
-import {ArtCreateComponent} from "./arts/art-create/art-create.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: ArtListComponent },
-  { path: 'create', component: ArtCreateComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
